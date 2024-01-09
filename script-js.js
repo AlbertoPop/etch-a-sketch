5ff091fc9ca10b4c787f3966eecb92931f357721
AlbertoPop <140503542+AlbertoPop@users.noreply.github.com>
2024-01-09 16:56:48 +0200

    changing size is working now, made hovering to color with black color for now

--- script-js.js
+++ script-js.js
@@ -1,19 +1,48 @@
-const container = document.querySelector('.container');
+const main = document.querySelector('.main');
+const setSize = document.querySelector('.set-size');
+const resetbtn = document.querySelector('.resetbtn');
 
 
 
-// 16x16 -> 256 boxes
-// size of 1 box -> 100%/256
-var input = prompt();
-var boxes = input * input ;
-
-for(let i = 1; i<= boxes; i++){
+var input = setSize.value;
+var container = document.createElement('div');
+container.setAttribute('class', 'container')
+main.appendChild(container);
+for(let i = 1; i<= input * input; i++){
     const div = document.createElement('div');
     div.setAttribute('class', 'square');
     div.style.width = `${100/input}%`;
     div.style.height = `${100/input}%`;
-
+    div.addEventListener('mouseover', function(){
+        div.style.backgroundColor = 'black';
+    })
     container.appendChild(div);
-
 }
+
+
+
+
+    
+setSize.addEventListener('click', function(){
+    container.remove();
+    container = document.createElement('div');
+    container.setAttribute('class', 'container')
+    main.appendChild(container);
+
+    input = setSize.value;
+    for(let i = 1; i<= input * input; i++){
+        const div = document.createElement('div');
+        div.setAttribute('class', 'square');
+        div.style.width = `${100/input}%`;
+        div.style.height = `${100/input}%`;
+        div.addEventListener('mouseover', function(){
+            div.style.backgroundColor = 'black';
+        })
+        container.appendChild(div);
+    }
+})
+
+resetbtn.addEventListener('click', function(){
+    window.location.reload();
+})
 
