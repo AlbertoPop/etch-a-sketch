ea923ad77b680cb8897f2a2d32e4f1322eb10c7e
AlbertoPop <140503542+AlbertoPop@users.noreply.github.com>
2024-01-16 17:49:28 +0200

    changing color is working, added rainbow. still unfinished.

--- script-js.js
+++ script-js.js
@@ -1,8 +1,9 @@
 const main = document.querySelector('.main');
 const setSize = document.querySelector('.set-size');
 const resetbtn = document.querySelector('.resetbtn');
-
-
+const colorbtn = document.querySelector('.colorbtn');
+const rainbowbtn = document.querySelector('.rainbowbtn')
+const colorlabel = document.querySelector('.colorlabel');
 
 var input = setSize.value;
 var container = document.createElement('div');
@@ -13,15 +14,16 @@ for(let i = 1; i<= input * input; i++){
     div.setAttribute('class', 'square');
     div.style.width = `${100/input}%`;
     div.style.height = `${100/input}%`;
+
     div.addEventListener('mouseover', function(){
-        div.style.backgroundColor = 'black';
+        div.style.backgroundColor = colorbtn.value;
     })
     container.appendChild(div);
 }
 
 
 
-
+//Setting new size/pixels
     
 setSize.addEventListener('click', function(){
     container.remove();
@@ -36,12 +38,34 @@ setSize.addEventListener('click', function(){
         div.style.width = `${100/input}%`;
         div.style.height = `${100/input}%`;
         div.addEventListener('mouseover', function(){
-            div.style.backgroundColor = 'black';
+            div.style.backgroundColor = colorbtn.value;
         })
         container.appendChild(div);
     }
 })
 
+rainbowbtn.addEventListener('click', function(){
+    container.remove();
+    colorbtn.remove();
+    colorlabel.remove();
+    container = document.createElement('div');
+    container.setAttribute('class', 'container')
+    main.appendChild(container);
+
+    input = setSize.value;
+    for(let i = 1; i<= input * input; i++){
+        const div = document.createElement('div');
+        div.setAttribute('class', 'square');
+        div.style.width = `${100/input}%`;
+        div.style.height = `${100/input}%`;
+        div.addEventListener('mouseover', function(){
+            div.style.backgroundColor = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
+        })
+        container.appendChild(div);
+    }
+})
+
+//Reset everything / page
 resetbtn.addEventListener('click', function(){
     window.location.reload();
 })
