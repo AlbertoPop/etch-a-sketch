bce4f27f6e7161183eaa74faada30b3fcd4713c6
AlbertoPop <140503542+AlbertoPop@users.noreply.github.com>
2024-01-19 15:04:30 +0200

    wroking version, updated design, rainbow mode is not perfect. might update.

--- script-js.js
+++ script-js.js
@@ -4,60 +4,65 @@ const resetbtn = document.querySelector('.resetbtn');
 const colorbtn = document.querySelector('.colorbtn');
 const rainbowbtn = document.querySelector('.rainbowbtn')
 const colorlabel = document.querySelector('.colorlabel');
+const square = document.querySelectorAll('.square');
+const sizeinfo = document.querySelector(`.size-info`);
 
-var input = setSize.value;
+
+//We create the container for the elements
+
 var container = document.createElement('div');
 container.setAttribute('class', 'container')
 main.appendChild(container);
-for(let i = 1; i<= input * input; i++){
-    const div = document.createElement('div');
-    div.setAttribute('class', 'square');
-    div.style.width = `${100/input}%`;
-    div.style.height = `${100/input}%`;
 
-    div.addEventListener('mouseover', function(){
-        div.style.backgroundColor = colorbtn.value;
-    })
-    container.appendChild(div);
+// Function to create elements for the table
+const createTable = function(){
+    for(let i = 1; i<= input * input; i++){
+        const div = document.createElement('div');
+        div.setAttribute('class', 'square');
+        div.style.width = `calc(${100/input}% - 1px`;
+        div.style.height = `calc(${100/input}% - 1px`;
+        div.addEventListener('mouseover', function(){
+            div.style.backgroundColor = colorbtn.value;
+        })
+        container.appendChild(div);
+        
+    }
 }
+
+// Function to clear table/delete all elements
+const removeTable = function(){
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
+
+// Creating default table
+var input = setSize.value; 
+sizeinfo.innerHTML = `Grid size: ${input} X ${input}`;
+createTable();
 
 
 
 //Setting new size/pixels
     
 setSize.addEventListener('click', function(){
-    container.remove();
-    container = document.createElement('div');
-    container.setAttribute('class', 'container')
-    main.appendChild(container);
-
+    removeTable();
     input = setSize.value;
-    for(let i = 1; i<= input * input; i++){
-        const div = document.createElement('div');
-        div.setAttribute('class', 'square');
-        div.style.width = `${100/input}%`;
-        div.style.height = `${100/input}%`;
-        div.addEventListener('mouseover', function(){
-            div.style.backgroundColor = colorbtn.value;
-        })
-        container.appendChild(div);
-    }
+    sizeinfo.innerHTML = `Grid size: ${input} X ${input}`;
+    createTable();
 })
 
 rainbowbtn.addEventListener('click', function(){
-    container.remove();
+    removeTable();
     colorbtn.remove();
     colorlabel.remove();
-    container = document.createElement('div');
-    container.setAttribute('class', 'container')
-    main.appendChild(container);
 
     input = setSize.value;
     for(let i = 1; i<= input * input; i++){
         const div = document.createElement('div');
         div.setAttribute('class', 'square');
-        div.style.width = `${100/input}%`;
-        div.style.height = `${100/input}%`;
+        div.style.width = `calc(${100/input}% - 1px`;
+        div.style.height = `calc(${100/input}% - 1px`;
         div.addEventListener('mouseover', function(){
             div.style.backgroundColor = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
         })
@@ -69,4 +74,3 @@ rainbowbtn.addEventListener('click', function(){
 resetbtn.addEventListener('click', function(){
     window.location.reload();
 })
-
